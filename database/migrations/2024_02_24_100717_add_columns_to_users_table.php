0222108ed80144ce54359d474c46da8e3be04042
DezyNation <87603422+DezyNation@users.noreply.github.com>
2024-02-24 15:40:41 +0530

    comm

--- database/migrations/2024_02_24_100717_add_columns_to_users_table.php
+++ database/migrations/2024_02_24_100717_add_columns_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->id('wallet_id')->after('id');
+            $table->integerIncrements('wallet_id')->after('id');
         });
 
         Schema::table('users', function (Blueprint $table) {
