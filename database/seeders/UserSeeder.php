4fb80a7b36da156279d9d893d8a38308c1a7e348
Rishi <rk3141508@gmail.com>
2024-04-10 20:19:24 +0530

    retailer

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,13 +17,15 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'capped_balance' => 0
         ])->assignRole('admin');
 
         User::create([
             'name' => 'Retailer',
             'email' => 'test@example.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'capped_balance' => 100
         ])->assignRole('retailer');
     }
 }
