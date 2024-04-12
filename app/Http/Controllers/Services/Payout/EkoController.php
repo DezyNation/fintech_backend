8cdc257cbb40b95174794c89b8430eb3f05d5091
Rishi <rk3141508@gmail.com>
2024-04-12 15:53:20 +0530

    log serv

--- app/Http/Controllers/Services/Payout/EkoController.php
+++ app/Http/Controllers/Services/Payout/EkoController.php
@@ -87,6 +87,8 @@ class EkoController extends Controller
         $response = Http::withHeaders($this->ekoHeaders())->asForm()
             ->put(config('services.eko.base_url') . '/v1/user/service/activate', $data);
 
+        Log::info($response);
+
         if ($response->failed()) {
             $this->releaseLock(auth()->user()->id);
             abort(403, $response['message'] ?? "Failed.");
