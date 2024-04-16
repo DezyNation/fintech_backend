006cb2a8d0e320d2e8ea6ce95f8706519a489401
Rishi <rk3141508@gmail.com>
2024-04-17 00:18:21 +0530

    logger

--- app/Http/Controllers/Services/Payout/EkoController.php
+++ app/Http/Controllers/Services/Payout/EkoController.php
@@ -105,6 +105,7 @@ class EkoController extends Controller
         $response = Http::withHeaders($this->ekoHeaders())
             ->get(config('services.eko.base_url') . "/v1/transactions/client_ref_id:$transaction_id", ['initiator_id' => config('services.eko.initiator_id')]);
 
+            Log::info($response);
         if ($response->failed()) {
             abort($response->status(), "Failed to fetch data. Please try again later.");
         }
