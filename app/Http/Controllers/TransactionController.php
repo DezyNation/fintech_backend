78ee972875237c3d582b1073ead344779ff0554c
Rishi <rk3141508@gmail.com>
2024-04-16 19:33:58 +0530

    trxn

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -13,7 +13,8 @@ class TransactionController extends Controller
     public static function store(User $user, string $reference_id, string $service, string $description, float $credit_amount, float $debit_amount, array $response = null)
     {
         $auth_user = auth()->user() ?? $user;
-        $closing_balance = $user->wallet + $credit_amount - $debit_amount;
+        $last_transaction = Transaction::where('user_id', $user->id)->latest()->first();
+        $closing_balance = $last_transaction->closing_balance + $credit_amount - $debit_amount;
         Transaction::create([
             'user_id' => $user->id,
             'updated_by' => $auth_user->id,
@@ -23,7 +24,7 @@ class TransactionController extends Controller
             'description' => $description,
             'credit_amount' => $credit_amount,
             'debit_amount' => $debit_amount,
-            'opening_balance' => $user->wallet,
+            'opening_balance' => $last_transaction->closing_balance,
             'closing_balance' => $closing_balance,
         ]);
 
