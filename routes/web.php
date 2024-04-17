2b069ebc7a6d4b76595c904a16405a800094359f
Rishi <rk3141508@gmail.com>
2024-04-17 12:00:44 +0530

    transaction

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\TransactionController;
+use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    TransactionController::store(User::find('9bd3b073-13bc-4848-9e16-51b8dcb81d5a'), 'PAY-661f681e4799a', 'payout', "Payout initiated", 0, 5000);
+    TransactionController::store(User::find('9bd3b073-13bc-4848-9e16-51b8dcb81d5a'), 'PAY-661f681e4799a', 'payout_commission', "Payout Commission", 0, 50);
+    TransactionController::store(User::find('9bd3b073-13bc-4848-9e16-51b8dcb81d5a'), 'PAY-661f68592f667', 'payout', "Payout initiated", 0, 5000);
+    TransactionController::store(User::find('9bd3b073-13bc-4848-9e16-51b8dcb81d5a'), 'PAY-661f68592f667', 'payout_commission', "Payout Commission", 0, 50);
     return [config('app.name') => 'Dezynation'];
 });
 
