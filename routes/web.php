ce753177b812e67df174ba8a4f1def4c00f3d71d
Rishi <rk3141508@gmail.com>
2024-04-21 14:52:30 +0530

    update pin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    User::where('email', 'k7037407204@gmail.com')->update(['pin' => Hash::make('1234')]);
     return [config('app.name') => 'Dezynation'];
 });
 
