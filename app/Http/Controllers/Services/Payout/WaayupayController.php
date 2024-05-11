d1c9199cbaab1ec1cb341fc4ac442bbda4b96fc5
Rishi <rk3141508@gmail.com>
2024-05-11 09:27:53 +0530

    ssl verify

--- app/Http/Controllers/Services/Payout/WaayupayController.php
+++ app/Http/Controllers/Services/Payout/WaayupayController.php
@@ -67,6 +67,7 @@ class WaayuPayController extends Controller
             'ifscCode' => $request->ifsc_code
         ];
         $response = Http::withToken($token)
+            ->withoutVerifying()
             ->post(config('services.waayupay.base_url') . '/payout/transaction', $data);
 
         return $this->processResponse($response, $response['status']);
