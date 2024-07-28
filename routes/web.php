ef72af620b35fa3ccea337db82c21651f66f6a98
Rishi <rk3141508@gmail.com>
2024-07-28 12:38:53 +0530

    web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\Services\DMT\EkoController;
-use App\Http\Controllers\Services\Payout\RblController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return [config('app.name') => 'Dezynation'];
 });
-
-Route::get('test', [RblController::class, 'initiateTransaction']);
 
 require __DIR__ . '/auth.php';
