ba7d85e21b8d65d50996282ade00930e246e2e1c
Rishi <rk3141508@gmail.com>
2024-11-15 17:03:49 +0530

    null safety

--- app/Http/Controllers/Services/Payout/SafexpayController.php
+++ app/Http/Controllers/Services/Payout/SafexpayController.php
@@ -13,7 +13,7 @@ class SafexpayController extends Controller
     public function processResponse($response): array
     {
         if (in_array($response->response->code, ["0001", "0000"])) {
-            $utr = ($response->payOutBean->bankRefNo == 'NA') ? null : $response->payOutBean->bankRefNo;
+            $utr = ($response->payOutBean?->bankRefNo == 'NA') ? null : $response->payOutBean->bankRefNo;
             $data = [
                 'status' => 'success',
                 'message' => $response->response->description,
