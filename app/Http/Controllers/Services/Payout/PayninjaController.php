eeba7a9eae72a7a918f244c649b2ef5687c32813
Rishi <rk3141508@gmail.com>
2025-03-03 16:55:40 +0530

    correct key

--- app/Http/Controllers/Services/Payout/PayninjaController.php
+++ app/Http/Controllers/Services/Payout/PayninjaController.php
@@ -16,7 +16,7 @@ class PayninjaController extends Controller
             $data = [
                 'status' => 'success',
                 'message' => $response['message'],
-                'utr' => $response['response']['utr'],
+                'utr' => $response['data']['utr'],
                 'transaction_status' => $response['data']['status']
             ];
         } else {
