1e9c16522e087ba9f867bd3c74e7c28b69c665e7
Rishi <rk3141508@gmail.com>
2025-03-04 16:39:57 +0530

    raw

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,14 @@ Route::get('/', function () {
     return [config('app.name') => 'Dezynation'];
 });
 
+Route::get('test', function () {
+    return openssl_decrypt(
+        base64_decode("2VInlsGFjRZ9+GR/7PrAYP2mzEPK2HyabIvyZF7EaBr3K0eOeJpszC5OnIiFVFUwPTs+yi7hfkkNp6YQWPuDeYrWEKHyNc2NdEbr+/sW6+DnhPTKi/5mAeWXXLEp5rSnolH3N/+cuQ1Cao3/QMceOw=="),
+        'AES-256-CBC',
+        config('services.payninja.decrypt_secret'),
+        OPENSSL_RAW_DATA,
+        'CYNfA9Ru9qUTbYta'
+    );
+});
+
 require __DIR__ . '/auth.php';
