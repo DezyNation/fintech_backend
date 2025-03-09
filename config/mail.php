8e6e5b76f46036d6b43e52513862d929137303d9
Rishi <rk3141508@gmail.com>
2025-03-09 22:14:51 +0530

    mail

--- config/mail.php
+++ config/mail.php
@@ -99,8 +99,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'support@dainypay.co.in'),
-        'name' => env('MAIL_FROM_NAME', 'JANPAY'),
+        'address' => env('MAIL_FROM_ADDRESS'),
+        'name' => env('MAIL_FROM_NAME'),
     ],
 
     /*
