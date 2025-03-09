e5115af224f934a8447244d3d5c040e7e5478513
Rishi <rk3141508@gmail.com>
2025-03-09 15:42:25 +0530

    env

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return [config('app.name') => 'Dezynation'];
+    return [config('app.name') => 'Dezynation India'];
 });
 
 require __DIR__ . '/auth.php';
