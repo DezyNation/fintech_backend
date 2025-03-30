a6fef1ae095fba992576fc1d0fe859a8e708d53c
Rishi <rk3141508@gmail.com>
2025-03-30 20:26:29 +0530

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'https://*.zeropay.info', 'http://localhost:3000', 'https://*.finoma.in'],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'https://*.zeropay.info', 'http://localhost:3000', 'https://*.finoma.in', 'https://*.dainypay.in'],
 
     'allowed_origins_patterns' => [],
 
