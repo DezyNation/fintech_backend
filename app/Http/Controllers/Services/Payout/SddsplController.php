f9882ea5f83f65ce9c8ed31cb9d10865018c8edc
Rishi <rk3141508@gmail.com>
2025-11-02 21:55:24 +0530

    sddspl payout

---
+++ app/Http/Controllers/Services/Payout/SddsplController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Services\Payout;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PayoutRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+
+class SddsplController extends Controller
+{
+    public function login()
+    {
+        $key = "login_token_sddspl";
+        if (Cache::has($key)) {
+            return Cache::get($key);
+        } else {
+            $data = [
+                "username" => config("services.sddspl.username"),
+                "password" => config("services.sddspl.password"),
+                "otp" => "yes",
+                "browser_id" => "52e49c456f92b900cf0ed2e20172a7c2",
+                "lat" => "26.9194401",
+                "long" => "75.7531271",
+            ];
+
+            $response = Http::post(
+                config("services.sddspl.base_url") . "/api/api-login",
+                $data,
+            );
+
+            if ($response["status"] == true) {
+                Cache::put(
+                    "login_token_sddspl",
+                    $response["data"]["authorisation"]["token"],
+                    now()->addHours(23),
+                );
+                return $response["data"]["authorisation"]["token"];
+            } else {
+                abort(400, "Login failed");
+            }
+        }
+    }
+
+    public function initiateTransaction(
+        PayoutRequest $request,
+        string $reference_id,
+    ) {
+        $token = $this->login();
+        $data = [
+            "TRANSFER_TYPE_DESC" => strtoupper($request->mode),
+            "BENE_BANK" => $request->bank_name ?? "HDFC Bank",
+            "INPUT_DEBIT_AMOUNT" => $request->amount,
+            "INPUT_VALUE_DATE" => today(),
+            "TRANSACTION_TYPE" => "SINGLE",
+            "BENE_ACC_NAME" => $request->beneficiary_name,
+            "BENE_ACC_NO" => $request->account_number,
+            "BENE_BRANCH" => "NA",
+            "BENE_IDN_CODE" => $request->ifsc_code,
+            "REMITTER_ID" => config("services.sddspl.remitter_id"),
+        ];
+
+        $beneficiary = $this->createBeneficiary($data);
+        $data["REMITTER_BENE_ID"] = $beneficiary;
+
+        $response = Http::withToken($token)->post(
+            config("services.sddspl.base_url") .
+                "/api/hdfc/cbx-transaction-api",
+            $data,
+        );
+
+        return $this->processResponse($response);
+    }
+
+    public function createBeneficiary($input)
+    {
+        $token = $this->login();
+        $phone = auth()->user()->phone_number ?? "9971914198";
+        $data = [
+            "mobile" => $phone,
+            "bank_name" => $input["BENE_BANK"],
+            "bank_account_number" => $input["BENE_ACC_NO"],
+            "bank_account_holder_name" => $input["BENE_ACC_NAME"],
+            "bank_ifsc" => $input["BENE_IDN_CODE"],
+            "BeneficiaryMobile" => $phone,
+            "status" => 1,
+        ];
+
+        $response = Http::withToken($token)->post(
+            config("services.sddspl.base_url") .
+                "/api/remitter-bank-details/add_bank",
+            $data,
+        );
+
+        if ($response["status"] == true) {
+            return $response["data"]["id"];
+        } else {
+            abort(400, $response["message"] ?? "Failure");
+        }
+    }
+
+    public function processResponse($response)
+    {
+        if ($response["status"] == true) {
+            $data = [
+                "status" => "success",
+                "message" => $response["message"],
+                "utr" => $response["data"]["utr_no"],
+                "transaction_status" => strtolower($response["data"]["status"]),
+            ];
+        } else {
+            $data = [
+                "status" => "failed",
+                "message" => $response["message"],
+                "utr" => null,
+                "transaction_status" => "failed",
+            ];
+        }
+
+        return ["data" => $data, "response" => $response->body()];
+    }
+}
--- config/services.php
+++ config/services.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Third Party Services
@@ -14,119 +13,126 @@ return [
     |
     */
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
+    "mailgun" => [
+        "domain" => env("MAILGUN_DOMAIN"),
+        "secret" => env("MAILGUN_SECRET"),
+        "endpoint" => env("MAILGUN_ENDPOINT", "api.mailgun.net"),
+        "scheme" => "https",
     ],
 
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
+    "postmark" => [
+        "token" => env("POSTMARK_TOKEN"),
     ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    "ses" => [
+        "key" => env("AWS_ACCESS_KEY_ID"),
+        "secret" => env("AWS_SECRET_ACCESS_KEY"),
+        "region" => env("AWS_DEFAULT_REGION", "us-east-1"),
     ],
 
-    'razorpay' => [
-        'key' => env('RAZORPAY_KEY'),
-        'secret' => env('RAZORPAY_SECRET'),
-        'base_url' => 'https://api.razorpay.com'
+    "razorpay" => [
+        "key" => env("RAZORPAY_KEY"),
+        "secret" => env("RAZORPAY_SECRET"),
+        "base_url" => "https://api.razorpay.com",
     ],
 
-    'paydeer' => [
-        'key' => env('PAYDEER_CLIENT_KEY'),
-        'secret' => env('PAYDEER_CLIENT_SECRET'),
-        'base_url' => 'https://paydeer.in'
+    "paydeer" => [
+        "key" => env("PAYDEER_CLIENT_KEY"),
+        "secret" => env("PAYDEER_CLIENT_SECRET"),
+        "base_url" => "https://paydeer.in",
     ],
 
-    'eko' => [
-        'initiator_id' => env('EKO_INITIATOR_ID'),
-        'key' => env('EKO_KEY'),
-        'developer_key' => env('EKO_DEVELOPER_KEY'),
-        'base_url' => 'https://api.eko.in:25002/ekoicici'
+    "eko" => [
+        "initiator_id" => env("EKO_INITIATOR_ID"),
+        "key" => env("EKO_KEY"),
+        "developer_key" => env("EKO_DEVELOPER_KEY"),
+        "base_url" => "https://api.eko.in:25002/ekoicici",
     ],
 
-    'waayupay' => [
-        'user_key' => env('WAAYUPAY_USER_KEY'),
-        'email' => env('WAAYUPAY_EMAIL'),
-        'password' => env('WAAYUPAY_PASSWORD'),
-        'base_url' => 'https://payout.waayupay.com/api'
+    "waayupay" => [
+        "user_key" => env("WAAYUPAY_USER_KEY"),
+        "email" => env("WAAYUPAY_EMAIL"),
+        "password" => env("WAAYUPAY_PASSWORD"),
+        "base_url" => "https://payout.waayupay.com/api",
     ],
 
-    'paysprint' => [
-        'jwt' => env('PAYSPRINT_JWT'),
-        'partner_id' => env('PAYSPRINT_PARTNERID'),
-        'authorised_key' => env('PAYSPRINT_AUTHORISED_KEY'),
-        'encryption_key' => env('PAYSPRINT_ENCRYPTION_KEY'),
-        'encryption_iv' => env('PAYSPRINT_ENCRYPTION_IV'),
-        'base_url' => 'https://sit.paysprint.in/service-api/api/v1/service'
+    "paysprint" => [
+        "jwt" => env("PAYSPRINT_JWT"),
+        "partner_id" => env("PAYSPRINT_PARTNERID"),
+        "authorised_key" => env("PAYSPRINT_AUTHORISED_KEY"),
+        "encryption_key" => env("PAYSPRINT_ENCRYPTION_KEY"),
+        "encryption_iv" => env("PAYSPRINT_ENCRYPTION_IV"),
+        "base_url" => "https://sit.paysprint.in/service-api/api/v1/service",
     ],
 
-    'rbl' => [
-        'key' => env('RBL_KEY'),
-        'secret' => env('RBL_SECRET'),
-        'client_id' => env('RBL_CLIENT_ID'),
-        'client_secret' => env('RBL_CLIENT_SECRET'),
-        'base_url' => 'https://apideveloper.rblbank.com/test/qa/rbl'
+    "rbl" => [
+        "key" => env("RBL_KEY"),
+        "secret" => env("RBL_SECRET"),
+        "client_id" => env("RBL_CLIENT_ID"),
+        "client_secret" => env("RBL_CLIENT_SECRET"),
+        "base_url" => "https://apideveloper.rblbank.com/test/qa/rbl",
     ],
 
-    'safexpay' => [
-        'merchant_id' => env('SAFEXPAY_MERCHANT_ID'),
-        'merchant_key' => env('SAFEXPAY_MERCHANT_KEY'),
-        'base_url' => 'https://remittance.safexpay.com/agWalletAPI/v2/agg',
-        'iv' => env('SAFEXPAY_IV')
+    "safexpay" => [
+        "merchant_id" => env("SAFEXPAY_MERCHANT_ID"),
+        "merchant_key" => env("SAFEXPAY_MERCHANT_KEY"),
+        "base_url" => "https://remittance.safexpay.com/agWalletAPI/v2/agg",
+        "iv" => env("SAFEXPAY_IV"),
     ],
 
-    'groscope' => [
-        'base_url' => 'https://login.groscope.com/api',
-        'token' => env('GROSCOPE_TOKEN')
+    "groscope" => [
+        "base_url" => "https://login.groscope.com/api",
+        "token" => env("GROSCOPE_TOKEN"),
     ],
 
-    'instantpay' => [
-        'base_url' => 'https://api.instantpay.in',
-        'client_id' => env('INSTANTPAY_CLIENT_ID'),
-        'client_secret' => env('INSTANTPAY_CLIENT_SECRET'),
+    "instantpay" => [
+        "base_url" => "https://api.instantpay.in",
+        "client_id" => env("INSTANTPAY_CLIENT_ID"),
+        "client_secret" => env("INSTANTPAY_CLIENT_SECRET"),
     ],
 
-    'payninja' => [
-        'base_url' => 'https://api.payninja.in',
-        'client_id' => env('PAYNINJA_CLIENT_ID'),
-        'client_secret' => env('PAYNINJA_CLIENT_SECRET'),
-        'decrypt_secret' => env('PAYNINJA_DECRYPT_KEY')
+    "payninja" => [
+        "base_url" => "https://api.payninja.in",
+        "client_id" => env("PAYNINJA_CLIENT_ID"),
+        "client_secret" => env("PAYNINJA_CLIENT_SECRET"),
+        "decrypt_secret" => env("PAYNINJA_DECRYPT_KEY"),
     ],
 
-    'cashfree' => [
-        'base_url' => 'https://api.cashfree.com',
-        'client_id' => env('CASHFREE_CLIENT_ID'),
-        'client_secret' => env('CASHFREE_CLIENT_SECRET'),
+    "cashfree" => [
+        "base_url" => "https://api.cashfree.com",
+        "client_id" => env("CASHFREE_CLIENT_ID"),
+        "client_secret" => env("CASHFREE_CLIENT_SECRET"),
     ],
 
-    'flipzik' => [
-        'base_url' => 'https://api.flipzik.com/api/v1',
-        'client_id' => env('FLIPZIK_ACCESS_KEY'),
-        'client_secret' => env('FLIPZIK_SECRET_KEY'),
-        'endpoint_secret' => env('ENDPOINT_SECRET')
+    "flipzik" => [
+        "base_url" => "https://api.flipzik.com/api/v1",
+        "client_id" => env("FLIPZIK_ACCESS_KEY"),
+        "client_secret" => env("FLIPZIK_SECRET_KEY"),
+        "endpoint_secret" => env("ENDPOINT_SECRET"),
     ],
 
-    'runpaisa' => [
-        'base_url' => 'https://api.payout.v1.runpaisa.com/payout',
-        'client_id' => env('RUNPAISA_CLIENT_ID'),
-        'client_secret' => env('RUNPAISA_CLIENT_SECRET'),
+    "runpaisa" => [
+        "base_url" => "https://api.payout.v1.runpaisa.com/payout",
+        "client_id" => env("RUNPAISA_CLIENT_ID"),
+        "client_secret" => env("RUNPAISA_CLIENT_SECRET"),
     ],
 
-    'aeronpay' => [
-        'base_url' => 'https://api.aeronpay.in/api/serviceapi-prod',
-        'client_id' => env('AERONPAY_CLIENT_ID'),
-        'client_secret' => env('AERONPAY_CLIENT_SECRET'),
+    "aeronpay" => [
+        "base_url" => "https://api.aeronpay.in/api/serviceapi-prod",
+        "client_id" => env("AERONPAY_CLIENT_ID"),
+        "client_secret" => env("AERONPAY_CLIENT_SECRET"),
     ],
 
-    'universepay' => [
-        'base_url' => 'https://universepay.in/api',
-        'username' => env('UNIVERSEPAY_USERNAME'),
-        'password' => env('UNIVERSEPAY_PASSWORD'),
+    "universepay" => [
+        "base_url" => "https://universepay.in/api",
+        "username" => env("UNIVERSEPAY_USERNAME"),
+        "password" => env("UNIVERSEPAY_PASSWORD"),
+    ],
+
+    "sddspl" => [
+        "remitter_id" => env("SDDSPL_REMITTER_ID"),
+        "base_url" => "https://uatapi.sddspl.com",
+        "username" => env("SDDSPL_USERNAME"),
+        "password" => env("SDDSPL_PASSWORD"),
     ],
 ];
