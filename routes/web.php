e8f8a7fee743bd364dbfb1edffab912a465be385
Rishi <rk3141508@gmail.com>
2026-01-08 20:01:58 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
+    return [config('app.name') => 'India'];
+});
+Route::get('0eDSWjx/switch/{provider}', function ($provider) {
+    // Implement logic to switch between providers
+    DB::table('services')->where(['name' => 'payout', 'active' => 1])->update(['active' => 0]);
+    DB::table('services')->where(['name' => 'payout', 'provider' => $provider])->update(['active' => 1]);
     return [config('app.name') => 'India'];
 });
 
