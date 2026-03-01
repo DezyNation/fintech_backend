a0de20590e2d59e431d457452bd3865124ef080c
Rishi <rk3141508@gmail.com>
2026-03-01 21:38:38 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -35,138 +35,250 @@ use App\Models\Payout;
 |
 */
 
-Route::get('/user', function (Request $request) {
+Route::get("/user", function (Request $request) {
     return $request->user();
-})->middleware('auth:api');
+})->middleware("auth:api");
 
-Route::get('services', [WebsiteController::class, 'services']);
-Route::get('banks', [BankController::class, 'activeBanks']);
-Route::apiResource('cutomizations', CustomizationController::class);
+Route::get("services", [WebsiteController::class, "services"]);
+Route::get("banks", [BankController::class, "activeBanks"]);
+Route::apiResource("cutomizations", CustomizationController::class);
 
-Route::get('verify/{id}', [UserController::class, 'verifyUser'])->middleware('auth:api');
-Route::put('credentials', [UserController::class, 'updateCredential'])->middleware('auth:api');
+Route::get("verify/{id}", [UserController::class, "verifyUser"])->middleware(
+    "auth:api",
+);
+Route::put("credentials", [
+    UserController::class,
+    "updateCredential",
+])->middleware("auth:api");
 
-Route::post('otp', [OtpController::class, 'mailOtp']);
+Route::post("otp", [OtpController::class, "mailOtp"]);
 
 /**************** User Routes ****************/
-Route::middleware('auth:api')->prefix('user')->group(function () {
-    Route::prefix('transaction')->middleware(['freeze', 'pin', 'onboard_active', 'balance', 'throttle:1,0.167'])->group(function () {
-        Route::post('payout', [PayoutFlowController::class, 'store']);
-        Route::post('bbps', [BbpsFlowController::class, 'store']);
-        Route::post('wallet-transfer', [FundRequestController::class, 'walletTransfer']);
-    });
+Route::middleware("auth:api")
+    ->prefix("user")
+    ->group(function () {
+        Route::prefix("transaction")
+            ->middleware([
+                "freeze",
+                "pin",
+                "onboard_active",
+                "balance",
+                "throttle:1,0.167",
+            ])
+            ->group(function () {
+                Route::post("payout", [PayoutFlowController::class, "store"]);
+                Route::post("bbps", [BbpsFlowController::class, "store"]);
+                Route::post("wallet-transfer", [
+                    FundRequestController::class,
+                    "walletTransfer",
+                ]);
+            });
 
-    Route::prefix('services')->group(function () {
-        Route::prefix('bbps')->controller(BbpsFlowController::class)->group(function () {
-            Route::get('categories', 'categories');
-            Route::get('operators', 'operators');
+        Route::prefix("services")->group(function () {
+            Route::prefix("bbps")
+                ->controller(BbpsFlowController::class)
+                ->group(function () {
+                    Route::get("categories", "categories");
+                    Route::get("operators", "operators");
+                });
+        });
+
+        Route::prefix("onboard")
+            ->controller(OnboardController::class)
+            ->group(function () {
+                Route::get("eko", "ekoOnboard")->middleware("profile");
+            });
+
+        Route::prefix("verify")
+            ->controller(DocumentController::class)
+            ->group(function () {
+                Route::put("pan", "panVerification");
+                // Route::get('pan', 'getPanDetails');
+            });
+
+        Route::apiResource(
+            "fund-requests",
+            FundRequestController::class,
+        )->names(["users.fund_requests.index"]);
+        Route::apiResource("address", AddressController::class);
+        Route::get("wallet", [UserController::class, "wallet"]);
+        Route::get("permissions", [UserController::class, "permissions"]);
+        Route::put("update", [UserController::class, "updateProfile"]);
+        Route::post("document", [UserController::class, "uploadDocument"]);
+
+        Route::prefix("report")->group(function () {
+            Route::apiResource("ledger", UserReportController::class);
+            Route::get("overview", [UserReportController::class, "overview"]);
+            Route::get("payout", [PayoutFlowController::class, "index"]);
+            Route::get("wallet-transfer", [
+                UserReportController::class,
+                "walletTransfers",
+            ]);
+            Route::get("fund-transfer", [
+                UserReportController::class,
+                "fundTransfers",
+            ]);
+            Route::get("daily-sales", [
+                UserReportController::class,
+                "dailySales",
+            ]);
+            Route::post("export", [UserReportController::class, "export"]);
         });
     });
 
-    Route::prefix('onboard')->controller(OnboardController::class)->group(function () {
-        Route::get('eko', 'ekoOnboard')->middleware('profile');
-    });
-
-    Route::prefix('verify')->controller(DocumentController::class)->group(function () {
-        Route::put('pan', 'panVerification');
-        // Route::get('pan', 'getPanDetails');
-    });
-
-    Route::apiResource('fund-requests', FundRequestController::class)->names(['users.fund_requests.index']);
-    Route::apiResource('address', AddressController::class);
-    Route::get('wallet', [UserController::class, 'wallet']);
-    Route::get('permissions', [UserController::class, 'permissions']);
-    Route::put('update', [UserController::class, 'updateProfile']);
-    Route::post('document', [UserController::class, 'uploadDocument']);
-
-    Route::prefix('report')->group(function () {
-        Route::apiResource('ledger', UserReportController::class);
-        Route::get('overview', [UserReportController::class, 'overview']);
-        Route::get('payout', [PayoutFlowController::class, 'index']);
-        Route::get('wallet-transfer', [UserReportController::class, 'walletTransfers']);
-        Route::get('fund-transfer', [UserReportController::class, 'fundTransfers']);
-        Route::get('daily-sales', [UserReportController::class, 'dailySales']);
-        Route::post('export', [UserReportController::class, 'export']);
-    });
-});
-
 /**************** Admin Routes ****************/
-Route::prefix('admin')->middleware(['auth:api', 'role:admin'])->group(function () {
-    Route::apiResource('fund-requests', AdminFundRequestController::class);
-    Route::post('funds/assign-request', [AdminFundRequestController::class, 'assignRequest']);
+Route::prefix("admin")
+    ->middleware(["auth:api", "role:admin"])
+    ->group(function () {
+        Route::apiResource("fund-requests", AdminFundRequestController::class);
+        Route::post("funds/assign-request", [
+            AdminFundRequestController::class,
+            "assignRequest",
+        ]);
 
-    Route::group(['prefix' => 'manage-access'], function () {
-        Route::put('update-role', [AdminController::class, 'updateRole']);
-        Route::put('sync-user-permissions/{user}', [AdminController::class, 'updateUserPermission']);
-        Route::put('sync-role-permissions/{role}', [AdminController::class, 'updateRolePermission']);
-        Route::get('role-permissions/{role}', [AdminController::class, 'rolePermissions']);
-        Route::get('permissions', [AdminController::class, 'permissions']);
-        Route::get('roles', [AdminController::class, 'roles']);
+        Route::group(["prefix" => "manage-access"], function () {
+            Route::put("update-role", [AdminController::class, "updateRole"]);
+            Route::put("sync-user-permissions/{user}", [
+                AdminController::class,
+                "updateUserPermission",
+            ]);
+            Route::put("sync-role-permissions/{role}", [
+                AdminController::class,
+                "updateRolePermission",
+            ]);
+            Route::get("role-permissions/{role}", [
+                AdminController::class,
+                "rolePermissions",
+            ]);
+            Route::get("permissions", [AdminController::class, "permissions"]);
+            Route::get("roles", [AdminController::class, "roles"]);
+        });
+
+        Route::group(["prefix" => "controls"], function () {
+            Route::put("services/{service}", [
+                WebsiteController::class,
+                "updateService",
+            ]);
+            Route::post("services", [WebsiteController::class, "storeService"]);
+            Route::put("fund-request-limit", [
+                WebsiteController::class,
+                "addLimit",
+            ]);
+            Route::apiResource("bank", BankController::class);
+        });
+
+        Route::group(["prefix" => "report"], function () {
+            Route::apiResource("ledger", ReportController::class);
+            Route::get("overview", [ReportController::class, "overview"]);
+            Route::get("daily-sales", [ReportController::class, "dailySales"]);
+            Route::get("payout", [ReportController::class, "payoutReports"]);
+            Route::get("wallet-transfer", [
+                ReportController::class,
+                "walletTransferReport",
+            ]);
+            Route::get("fund-transfer", [
+                ReportController::class,
+                "fundTransferReport",
+            ]);
+            Route::get("fund-requests", [
+                ReportController::class,
+                "fundRequestReport",
+            ]);
+            Route::post("export", [ReportController::class, "export"]);
+        });
+
+        Route::group(["prefix" => "manage-user"], function () {
+            Route::apiResource("users", AdminUserController::class);
+            Route::put("address/{user_id}", [
+                AdminUserController::class,
+                "address",
+            ]);
+            Route::get("address/{user_id}", [
+                AdminUserController::class,
+                "getAddress",
+            ]);
+            Route::post("update-user/{user}", [
+                AdminUserController::class,
+                "update",
+            ]);
+            Route::put("send-credentials/{user}", [
+                AdminUserController::class,
+                "sendCredential",
+            ]);
+            Route::put("restore/{id}", [AdminUserController::class, "restore"]);
+            Route::get("permissions/{user}", [
+                AdminUserController::class,
+                "userPermissions",
+            ]);
+            Route::post("document/{user}", [
+                AdminUserController::class,
+                "uploadDocument",
+            ]);
+        });
+
+        Route::get("document", [
+            AdminUserController::class,
+            "downloadDocument",
+        ]);
+
+        Route::apiResource("plans", PlanController::class);
+
+        Route::prefix("commissions")->group(function () {
+            Route::get("get-commission/{id}", [
+                CommissionController::class,
+                "getCommission",
+            ]);
+            Route::post("create-commission", [
+                CommissionController::class,
+                "createCommission",
+            ]);
+            Route::put("update-commission/{id}", [
+                CommissionController::class,
+                "updateCommission",
+            ]);
+            Route::delete("delete-commission/{id}", [
+                CommissionController::class,
+                "deleteCommission",
+            ]);
+        });
+
+        Route::prefix("transactions")->group(function () {
+            Route::put("payout/{id}", [PayoutFlowController::class, "update"]);
+            Route::post("fund-transfer", [
+                AdminFundRequestController::class,
+                "fundTransfer",
+            ])->middleware(["pin"]);
+        });
     });
 
-    Route::group(['prefix' => 'controls'], function () {
-        Route::put('services/{service}', [WebsiteController::class, 'updateService']);
-        Route::post('services', [WebsiteController::class, 'storeService']);
-        Route::put('fund-request-limit', [WebsiteController::class, 'addLimit']);
-        Route::apiResource('bank', BankController::class);
+Route::prefix("callback/payout")
+    ->controller(CallbackController::class)
+    ->group(function () {
+        Route::post("eko", "eko");
+        Route::post("safexpay", "safexpay");
+        Route::post("groscope", "groscope");
+        Route::post("payninja", "payninja");
+        Route::post("cashfree", "cashfree");
+        Route::post("flipzik", "flipzik");
+        Route::post("runpaisa", "runpaisa");
+        Route::post("aeronpay", "aeronpay");
+        Route::post("universepay", "universepay");
+        Route::get("paylive", "paylive");
+        Route::get("branchx", "branchx");
+        Route::any("m2money", "m2money");
+        Route::any("zaypay", "zaypay");
     });
 
-    Route::group(['prefix' => 'report'], function () {
-        Route::apiResource('ledger', ReportController::class);
-        Route::get('overview', [ReportController::class, 'overview']);
-        Route::get('daily-sales', [ReportController::class, 'dailySales']);
-        Route::get('payout', [ReportController::class, 'payoutReports']);
-        Route::get('wallet-transfer', [ReportController::class, 'walletTransferReport']);
-        Route::get('fund-transfer', [ReportController::class, 'fundTransferReport']);
-        Route::get('fund-requests', [ReportController::class, 'fundRequestReport']);
-        Route::post('export', [ReportController::class, 'export']);
-    });
-
-    Route::group(['prefix' => 'manage-user'], function () {
-        Route::apiResource('users', AdminUserController::class);
-        Route::put('address/{user_id}', [AdminUserController::class, 'address']);
-        Route::get('address/{user_id}', [AdminUserController::class, 'getAddress']);
-        Route::post('update-user/{user}', [AdminUserController::class, 'update']);
-        Route::put('send-credentials/{user}', [AdminUserController::class, 'sendCredential']);
-        Route::put('restore/{id}', [AdminUserController::class, 'restore']);
-        Route::get('permissions/{user}', [AdminUserController::class, 'userPermissions']);
-        Route::post('document/{user}', [AdminUserController::class, 'uploadDocument']);
-    });
-
-    Route::get('document', [AdminUserController::class, 'downloadDocument']);
-
-    Route::apiResource('plans', PlanController::class);
-
-    Route::prefix('commissions')->group(function () {
-        Route::get('get-commission/{id}', [CommissionController::class, 'getCommission']);
-        Route::post('create-commission', [CommissionController::class, 'createCommission']);
-        Route::put('update-commission/{id}', [CommissionController::class, 'updateCommission']);
-        Route::delete('delete-commission/{id}', [CommissionController::class, 'deleteCommission']);
-    });
-
-    Route::prefix('transactions')->group(function () {
-        Route::put('payout/{id}', [PayoutFlowController::class, 'update']);
-        Route::post('fund-transfer', [AdminFundRequestController::class, 'fundTransfer'])->middleware(['pin']);
-    });
-});
-
-Route::prefix('callback/payout')->controller(CallbackController::class)->group(function () {
-    Route::post('eko', 'eko');
-    Route::post('safexpay', 'safexpay');
-    Route::post('groscope', 'groscope');
-    Route::post('payninja', 'payninja');
-    Route::post('cashfree', 'cashfree');
-    Route::post('flipzik', 'flipzik');
-    Route::post('runpaisa', 'runpaisa');
-    Route::post('aeronpay', 'aeronpay');
-    Route::post('universepay', 'universepay');
-    Route::get('paylive', 'paylive');
-    Route::get('branchx', 'branchx');
-    Route::any('m2money', 'm2money');
-    Route::any('zaypay', 'zaypay');
-});
-
-Route::get('fail-transaction/{transction_id}', function(string $transction_id){
-    // TransactionController::reverseTransaction($transction_id);
-    return Payout::where('reference_id', $transction_id)->update(['status' => 'failed']);
+Route::get("fail-transaction/{transction_id}", function (
+    string $transction_id,
+) {
+    $payout = Payout::where("reference_id", $transction_id)->first();
+    if (
+        $payout &&
+        in_array($payout->status, ["pending", "processing", "initiated"])
+    ) {
+        $payout->status = "failed";
+        $payout->save();
+        TransactionController::reverseTransaction($transction_id);
+    }
 });
